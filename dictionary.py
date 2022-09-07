935966eac9906afd7ea08ae15e9c881772b9d88f
nickgit89 <nick.schoettle@icloud.com>
2022-09-07 17:35:50 -0400

    new file:   dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+Namesofpeople = {
+    "First Friend": "Sam", 
+    "Best Friend": "Nich", 
+    "me": "nick"
+    }
+
+namesofpeoplelist = list(Namesofpeople)
+namesofpeoplelist.pop()
+
+print(namesofpeoplelist)
