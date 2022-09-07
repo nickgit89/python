e4a460957d0fdb44ab97ccd8a08cb5bc3e4c5d69
nickgit89 <nick.schoettle@icloud.com>
2022-09-07 17:25:44 -0400

    modified:   function.py

--- function.py
+++ function.py
@@ -1,2 +1,15 @@
 import netmiko
 
+from netmiko import ConnectHandler
+
+connection = ConnectHandler(
+    device_type="cisco_xe",
+    host="cisco5.domain.com",
+    username="admin",
+    password="password",
+)
+
+connection
+
+output = connection.send_command("show ip interface brief")
+print(output)
