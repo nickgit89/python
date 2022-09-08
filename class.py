d3ca50e0dabc177976989004d05253a9857096ff
nickgit89 <nick.schoettle@icloud.com>
2022-09-08 17:19:01 -0400

    modified:   class.py

--- class.py
+++ class.py
@@ -1,10 +1,11 @@
-class(hair)
-    def __init__(self, color, length, texture)
+class hair:
+    def __init__(self, color, length, texture):
         self.color = color
         self.length = length
         self.texture = texture
     
-    def cuthair(self, str="clip, clip, clip")
+    def cuthair(self, str="clip, clip, clip"):
         print(str)
-    
+
+myhair = hair(brown,long,straight)
     
