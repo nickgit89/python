6a8a84aabb7b3149b1148d0c035ef017632a5341
nickgit89 <nick.schoettle@icloud.com>
2022-09-08 17:13:16 -0400

    new file:   class.py

---
+++ class.py
@@ -0,0 +1,10 @@
+class(hair)
+    def __init__(self, color, length, texture)
+        self.color = color
+        self.length = length
+        self.texture = texture
+    
+    def cuthair(self, str="clip, clip, clip")
+        print(str)
+    
+    
