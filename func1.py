61adb83730a079987ea67b14d893af5467f5fb76
nickgit89 <nick.schoettle@icloud.com>
2022-09-15 21:29:46 -0400

    new file:   func1.py

---
+++ func1.py
@@ -0,0 +1,22 @@
+def func1():
+    print("I am a function.")
+
+
+func1()
+print(func1())
+print(func1)
+
+
+def func2(arg1, arg2):
+    print (arg1," ", arg2)
+
+def cube(x):
+    return x * x * x
+
+
+func2(10, 30)
+print(func2(10, 30))
+
+
+print(cube(3))
+
