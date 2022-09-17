4d6dff2f2da180231c1f34d33091cc53188f89c6
nickgit89 <nick.schoettle@icloud.com>
2022-09-17 06:43:01 -0400

    new file:   set.py 	new file:   tuple.py

---
+++ set.py
@@ -0,0 +1,7 @@
+randomletters = {"h", "a", "r"}
+
+randomletters.add('d')
+
+randomletters.discard("h")
+
+print(randomletters)
---
+++ tuple.py
@@ -0,0 +1,5 @@
+animals = ("dog", "cat", "lion", "panther")
+
+moreanimals = "panther", "alligator"
+
+print(type(moreanimals))
