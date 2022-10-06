00de4a91ea738de94aae8f7deb49d27e8e386a0f
nickgit89 <nick.schoettle@icloud.com>
2022-10-05 20:44:22 -0400

    new file:   falafal.py

---
+++ falafal.py
@@ -0,0 +1,22 @@
+from re import X
+
+
+x=-1
+while x <= 5:
+    print(x)
+    x += 1
+
+
+
+trees = ["maple", "oak", "pine"]
+
+pine= trees.pop()
+
+print(pine)
+
+trees.insert[4, "willow"]
+
+print[trees]
+
+
+
