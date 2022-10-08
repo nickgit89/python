73556aba3902d16bbe7c52dbd4d7086a4d9df5b3
nickgit89 <nick.schoettle@icloud.com>
2022-10-08 10:01:46 -0400

    new file:   wallet.py

---
+++ wallet.py
@@ -0,0 +1,17 @@
+fav_movies = ["spiderman", "forest gump", "hulk"]
+
+print(len(fav_movies))
+
+fav_movies.append("hulk2")
+
+print(fav_movies)
+
+fav_movies.insert(1, "lego movie")
+
+print(fav_movies)
+
+del(fav_movies[4])
+
+print(fav_movies)
+
+fav_movies.remove("forest gump")
