999ce108f98d1494ae631cdf8252136d00d8d553
nickgit89 <nick.schoettle@icloud.com>
2022-10-08 10:17:10 -0400

    new file:   all.py

---
+++ all.py
@@ -0,0 +1,16 @@
+all(['asdf', "asd"])
+
+myList = ["a", "b", "c", "d"]
+
+mylist= list(range(100))
+filteredList = [item for item in mylist if item % 2 == 0]
+print(filteredList)
+
+
+mystring = ("my name is nick. My last initial is \'s\'")
+
+
+
+mystring.split(".")
+
+print(mystring)
