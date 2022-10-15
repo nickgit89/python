ff2e3a58a3d5682d1d73d55f0eebf5e1d1b6819a
nickgit89 <nick.schoettle@icloud.com>
2022-10-15 12:45:45 -0400

    modified:   money.py

--- money.py
+++ money.py
@@ -12,3 +12,12 @@ governments = {"democracy": "freedom", "dictatorship": "control"}
 
 for government in governments:
     print(f"{government.title()} is a type of government")
+
+
+soccerteams = ["liverpool", "wrexham", "FC"]
+
+print(soccerteams[1])
+
+message = f"I'm watching a show about {soccerteams[1].title()}"
+
+print(message)
