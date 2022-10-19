f5afb689876542ee267f5657e3dd8157c29c891e
nickgit89 <nick.schoettle@icloud.com>
2022-10-19 18:17:01 -0400

    new file:   listprac.py

---
+++ listprac.py
@@ -0,0 +1,14 @@
+favorite_foods = ["pizza", "hamburgers", "pepperoni", "chipotle"]
+
+best_foods = favorite_foods[:]
+
+print(best_foods)
+
+cool_foods = best_foods
+
+favorite_foods.append("french fries")
+
+print(f"Best foods list: {best_foods}")
+print(f"Favorite Foods: {favorite_foods}")
+print(f"cool foods: {cool_foods}") 
+
