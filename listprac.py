7d72f5bddaf1d8c6f718a20bfff798a56d885db7
nickgit89 <nick.schoettle@icloud.com>
2022-10-29 12:01:05 -0400

    modified:   __pycache__/datetime.cpython-310.pyc 	modified:   listprac.py

--- listprac.py
+++ listprac.py
@@ -8,7 +8,11 @@ cool_foods = best_foods
 
 favorite_foods.append("french fries")
 
+favorite_foods.append("gelato")
+
 print(f"Best foods list: {best_foods}")
 print(f"Favorite Foods: {favorite_foods}")
 print(f"cool foods: {cool_foods}") 
+
+
 
