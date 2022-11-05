e59543e43a56072338c3719285545330b56be66f
nickgit89 <nick.schoettle@icloud.com>
2022-11-05 05:49:25 -0400

    new file:   loops.py

---
+++ loops.py
@@ -0,0 +1,11 @@
+counting = 10
+while counting > 0:
+    print( """ ^ 
+             <    >""" * counting)
+    counting -=1
+
+
+
+for x in range(0, 10):
+    print( """ ^
+             <    >""" * x)
