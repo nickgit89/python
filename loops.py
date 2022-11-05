6918007603b0d657c4c4b6fd818531a15a7594d2
nickgit89 <nick.schoettle@icloud.com>
2022-11-05 05:57:21 -0400

    modified:   loops.py

--- loops.py
+++ loops.py
@@ -9,3 +9,18 @@ while counting > 0:
 for x in range(0, 10):
     print( """ ^
              <    >""" * x)
+
+
+Expressions = ["Hello there!", "Whats your name", "How is it going"]
+
+for expression in Expressions:
+    print(expression)
+
+for x in range (0, 20):
+    if x % 2 == 0:
+        continue
+    if x == 13:
+        break
+        print("In IF:", x)
+    
+    print(x)
