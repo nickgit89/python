9ec4a04b5563e72d708abc648e618472d8c310fe
nickgit89 <nick.schoettle@icloud.com>
2022-11-13 13:40:49 -0500

    modified:   __pycache__/datetime.cpython-310.pyc 	deleted:    datetime.py 	new file:   pendelum.py

--- datetime.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from datetime import datetime
-
-now = datetime.now()
-
-print(now.strftime("The current year is: %Y"))
-
-
-print(now.strftime("%a, %d, %B, %y"))
---
+++ pendelum.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import time
+import pendulum
+
+datetime1 = pendulum.datetime(2022, 11, 13, tz= "America/Chicago")
+print(datetime1)
+print(isinstance(datetime1, datetime))
+
+print(datetime1.timezone.name)
+
+
+datetime2= datetime1.in_timezone("Europe/London")
+
+
+datetime3 = pendulum.now()
+print(datetime3)
+print(datetime3.timezone.name)
+
+
