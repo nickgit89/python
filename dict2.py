d8508c77fd36a5ba3158beceb1e6219c6a5b36ed
nickgit89 <nick.schoettle@icloud.com>
2022-11-19 10:29:00 -0500

    new file:   dict2.py

---
+++ dict2.py
@@ -0,0 +1,11 @@
+Christmasitems = {"person": "Santa", "transportation": "sleigh", "candy": "candy canes"}
+Christmasitems["food"] = "peppermint"
+print(Christmasitems)
+
+Christmasitems.update({"transportation": "reindeer"})
+
+Christmasitems.pop("food")
+
+Christmasitems["drink"] = "eggnog"
+
+print(Christmasitems)
