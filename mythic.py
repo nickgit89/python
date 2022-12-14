b7ccbe4066a43957af3c9fa1e66b5610b3282bba
nickgit89 <nick.schoettle@icloud.com>
2022-12-13 21:17:56 -0500

    new file:   mythic.py

---
+++ mythic.py
@@ -0,0 +1,14 @@
+mythic = {'type': 'game', 'source': 'show', 'age': 'medieval'}
+
+mythic.update({'source':'video game'})
+
+print(mythic)
+
+#mythic.add({'subscription':'paid'})
+print(mythic)
+
+del mythic['type']
+mythic.pop('age')
+mythic['service']='Appletv'
+
+print(mythic)
