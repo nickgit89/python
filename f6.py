2911d401a82477ef8214ff00b8f45790317509f5
nickgit89 <nick.schoettle@icloud.com>
2022-12-25 20:39:40 -0500

    new file:   f6.py

---
+++ f6.py
@@ -0,0 +1,7 @@
+agents = ["Jack Ryan", "John Wick", "Jason Bourne", "James Bond"]
+careers = ["CIA agent", "vigilante", "past CIA", "Mi6"]
+
+for x in range(len(agents)):
+    agent = agents[x]
+    career = careers[x]
+    print(f"{agent}: {career}")
