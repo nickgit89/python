5cdcde78789a91526fb29799fe5b508de440587f
nickgit89 <nick.schoettle@icloud.com>
2022-12-27 21:32:17 -0500

    modified:   condstate.py

--- condstate.py
+++ condstate.py
@@ -4,6 +4,18 @@ match value:
             result = 1
         case "two":
             result = 2
-        case "three" | "four":
-            result = (3,4)
+        case "three":
+            result = 3
+result = 3
 print(result)
+
+addition = ()
+match addition:
+    case "if it is today":
+        result = "then we go through with it"
+    case "if it is tomorrow":
+        result = "better we hold off"
+
+print(result)
+
+
